9b49dd3e679355f0149c356fb83f0c23c564affe
sburgosp <97459021+sburgosp@users.noreply.github.com>
2022-09-07 19:21:12 +0200

    Update Goby.py

--- Goby.py
+++ Goby.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 st.title('GoBY')
-st.subheader('Save Time   Having the cheapest price   Making your life easier')
+st.subheader('Save Time, Having the cheapest price, Making your life easier')
 image=Image.open('Uber.png')
 image2=Image.open('Cabify.png')
 image3=Image.open('Bolt.png')
